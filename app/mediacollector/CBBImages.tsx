85ba774543ddc77decdaa09c887ff97d4d17d199
Ben <benknox480@gmail.com>
2025-12-31 07:11:43 -0500

    fixed mistake

--- app/mediacollector/CBBImages.tsx
+++ app/mediacollector/CBBImages.tsx
@@ -90,7 +90,9 @@ export default function CBBImages({
           ></img>
 
           <div
-            className={`pointer-events-none absolute inset-0 flex content-center items-center opacity-0`}
+            className={`pointer-events-none absolute inset-0 flex content-center items-center ${
+              clicked[idx] ? 'opacity-100' : 'opacity-0'
+            }`}
           >
             <p className='-translate-x-1 -rotate-65 font-["Just_Another_Hand"] text-5xl font-bold tracking-wider text-[rgb(0,77,0)]'>
               Selected
