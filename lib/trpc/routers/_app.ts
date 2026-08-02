d8b617d99cbc9f77bf4039cc98e86ac8d32f8286
ben-from-earth <benknox480@gmail.com>
2026-08-02 17:21:55 -0400

    missed a type error

--- lib/trpc/routers/_app.ts
+++ lib/trpc/routers/_app.ts
@@ -2,7 +2,6 @@ import { router } from 'lib/trpc/trpc';
 import { genresRouter } from './genres/_';
 import { onlineRouter } from './online';
 import { pngRouter } from './png';
-import { profileRouter } from './profile';
 import { healthRouter } from './health';
 import { databaseRouter } from './database/_';
 import { collectRouter } from './collect/_';
@@ -13,7 +12,6 @@ export const appRouter = router({
   genres: genresRouter,
   online: onlineRouter,
   png: pngRouter,
-  profile: profileRouter,
   health: healthRouter,
 });
 
